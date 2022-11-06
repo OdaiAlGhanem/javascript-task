d0b242a33f135c457cffab5490a950ce4a0da17f
OdaiAlGhanem <88395007+OdaiAlGhanem@users.noreply.github.com>
2022-11-06 16:59:03 +0300

    index file

---
+++ task 6-11/task2/js.js
@@ -0,0 +1,10 @@
+let y=1;
+for( let i=1; i<=4; i++)  
+{  
+    for(let x=1; x<=i; x++)  
+    {  
+        document.write(y);
+        y++;
+    }  
+    document.write("<br>");
+}
---
+++ task 6-11/task3/js.js
@@ -0,0 +1,6 @@
+for (x = 1; x <= 1000; x++) {
+    if (x % 13 == 0) {
+        document.write(x + "<br>");
+    }
+
+}
