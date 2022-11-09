3c6eaab1e699a3e4f1d109e515baaeb621328350
OdaiAlGhanem <88395007+OdaiAlGhanem@users.noreply.github.com>
2022-11-10 00:18:59 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,80 +1,30 @@
-console.log();
-function submit1() {
 
-    localStorage.setItem("name", document.getElementById("name").value);
-    let Name = localStorage.getItem("name");
+function fun11()
+{
 
-    localStorage.setItem("age", document.getElementById("age").value);
-    let Age = localStorage.getItem("age");
+    sessionStorage.setItem("subject" ,document.getElementById("subject").value);
+    let subject=sessionStorage.getItem("subject");
 
-    localStorage.setItem("male".getElementById("male").value);
-    let Male = localStorage.getItem("male").checked;
+    sessionStorage.setItem("Description",document.getElementById("desc").value);
+    let dascription=sessionStorage.getItem("Description");
 
-    localStorage.setItem("female".getElementById("female").value);
-    let Female = localStorage.getItem("female").checked;
+    sessionStorage.setItem("userTypes",document.getElementById("ut").value);
+    let userTypes=sessionStorage.getItem("userTypes");
 
 
-    localStorage.setItem("descrep".getElementById("desc").value);
-    let Descrep = localStorage.getItem("desc");
+    sessionStorage.setItem("Technology" ,document.getElementById("tec").value);
+    let Technology=sessionStorage.getItem("Technology");
 
-    localStorage.setItem("univirsity".getElementById("uni").value);
-    let Uni = localStorage.getItem("uni").checked;
-
-    localStorage.setItem("javascript".getElementById("lang").value);
-    let Javascript = localStorage.getItem("lang1").checked;
-
-    localStorage.setItem("c++".getElementById("lang2").value);
-    let C = localStorage.getItem("lang2").checked;
-
-    localStorage.setItem("java".getElementById("lang3").value);
-    let Java = localStorage.getItem("lang3").checked;
+document.getElementById("Div1").style.display="block";
+document.getElementById("Div1").style.backgroundColor="yellow";
+document.getElementById("Div1").style.width="350px";
+document.getElementById("Div1").style.height="200px";
+document.getElementById("Div1").style.border="1px solid balck";
 
 
-    document.getElementById("Div1").style.display = "block";
-    document.getElementById("p1").innerHTML += Name;
+document.getElementById("p1").innerHTML+= subject;
+document.getElementById("p2").innerHTML+= dascription;
+document.getElementById("p3").innerHTML+= userTypes;
+document.getElementById("p4").innerHTML+= Technology;
 
-    document.getElementById("p2").innerHTML += Age;
-    var x = document.getElementById("male").value;
-    var y = document.getElementById("female").value;
-
-    if (x == "male")
-        document.getElementById("p3").innerHTML += x;
-    else
-        document.getElementById("p3").innerHTML += y;
-
-    document.getElementById("p4").innerHTML += Description;
-
-    document.getElementById("p5").innerHTML += major;
-
-
-    var a = document.getElementById("lang1");
-
-    if (a.checked) {
-        document.getElementById("l1").innerHTML += html;
-
-    }
-    else {
-        document.getElementById("lang1").style.display = "none";
-    }
-
-    var b = document.getElementById("lang2");
-
-    if (b.checked) {
-        document.getElementById("l2").innerHTML += c;
-
-    }
-    else {
-        document.getElementById("lang2").style.display = "none";
-    }
-
-    var z = document.getElementById("lang3");
-
-    if (z.checked) {
-        document.getElementById("l3").innerHTML += z.id;
-
-    }
-    else {
-        document.getElementById("lang3").style.display = "none";
-    }
 }
-
